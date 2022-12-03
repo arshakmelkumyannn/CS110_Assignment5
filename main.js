dade49efc4997cf534a74d07d3fd58443c888a87
Arshak Melkumyan <arshakmelkumyan0@gmail.com>
2022-12-03 18:31:01 +0400

    minor fixes

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 const utils = require("./utils.js");
 const prompt = require("readline-sync");
-console.log(utils);
 console.log(
-    "The product of matrices is:" +
-        utils.matrixProduct(
+    utils.matrixProduct(
             [
                 [1, 2, 3],
                 [4, 5, 6],
@@ -16,20 +14,7 @@ console.log(
             ]
         )
 );
-// console.log(
-//     typeof [
-//         [1, 2, 3],
-//         [4, 5, 6],
-//         [7, 8, 9],
-//     ],
-//     [
-//         [9, 8, 7],
-//         [6, 5, 4],
-//         [3, 2, 1],
-//     ]
-// );
 console.log(
-    "The sum of matrices is:" +
         utils.sumOfMatrices(
             [
                 [1, 2, 3],
@@ -44,7 +29,6 @@ console.log(
         )
 );
 console.log(
-    "The array for sum of each row is:" +
         utils.sumOfEachRow([
             [1, 2, 3],
             [4, 5, 6],
@@ -55,6 +39,6 @@ console.log("\n");
 const sentence = prompt.question(
     "Insert the sentence to see the word count.\n"
 );
-console.log("The word count is:" + utils.countWords(sentence));
+console.log("The word count is: " + utils.countWords(sentence));
 const string = prompt.question("Insert the string that needs to be trimmed.\n");
-console.log("The trimmed string is:" + utils.trim(string));
+console.log("The trimmed string is: " + utils.trim(string));
--- utils.js
+++ utils.js
@@ -1,22 +1,19 @@
 function matrixProduct(mat1, mat2) {
     let result = [];
 
-    //loop for c in mat1[c][x] and result[c][x]
     for (let c = 0; c < mat1.length; c++) {
         let row = [];
 
-        //loop for b in result[c][b] and mat2[x][b]
-        for (let b = 0; b < mat2[0].length; b++) {
+        for (let b = 0; b < mat2[c].length; b++) {
             let el = 0;
 
-            //loop for a in mat1[c][a] and mat2[a][b]
-            for (let a = 0; a < mat1[0].length; a++) {
+            for (let a = 0; a < mat1[c].length; a++) {
                 el += mat1[c][a] * mat2[a][b];
             }
             row.push(el);
         }
-
         result.push(row);
+        
     }
     return result;
 }
@@ -26,14 +23,19 @@ function matrixProduct(mat1, mat2) {
 
 function sumOfMatrices(mat1, mat2) {
     let result = [];
+
     for (let r = 0; r < mat1.length; r++) {
         let row = [];
-        for (let c = 0; c < mat1[0].length; c++) {
+
+        for (let c = 0; c < mat1[r].length; c++) {
             let el = mat1[r][c] + mat2[r][c];
+
             row.push(el);
         }
+
         result.push(row);
     }
+
     return result;
 }
 
@@ -42,17 +44,21 @@ function sumOfMatrices(mat1, mat2) {
 
 function sumOfEachRow(mat) {
     let result = [];
+
     for (let r = 0; r < mat.length; r++) {
-        result.push(sumOfArray(mat[r]));
+        result.push(_sumOfArray(mat[r]));
     }
+
     return result;
 }
 
-function sumOfArray(arr) {
+function _sumOfArray(arr) {
     let result = 0;
+
     for (let i = 0; i < arr.length; i++) {
         result += arr[i];
     }
+
     return result;
 }
 
@@ -60,21 +66,24 @@ function countWords(str) {
     let newStr = " " + str;
     let count = 0;
     let isWord = 0;
-    for (let n = 0; n < str.length; n++) {
-        if (isWord === 0 && str[n] !== " ") {
+
+    for (let n = 0; n < newStr.length; n++) {
+        if (isWord === 0 && newStr[n] !== " ") {
             count++;
             isWord = 1;
         }
-        if (str[n] === " ") {
+        if (newStr[n] === " ") {
             isWord = 0;
         }
     }
+
     return count;
 }
 
 function trim(str) {
     let leadi = 0;
     let traili = 0;
+
     for (let i = 0; i < str.length; i++) {
         if (str[i] !== " ") {
             leadi = i;
@@ -83,11 +92,12 @@ function trim(str) {
     }
     for (let i = str.length - 1; i >= 0; i--) {
         if (str[i] !== " ") {
-            traili = i + 1;
+            traili = i;
             break;
         }
     }
-    return str.slice(leadi, traili);
+
+    return str.slice(leadi, traili + 1); 
 }
 
 module.exports = {
